7170f18ed59f247318f8b1c00b34a260a2ebb288
Michael Dudley <michael.dudley@improvingenterprises.com>
2013-11-20 01:00:33 -0600

    Implemented the simple example

---
+++ DurandalPlugins/DurandalPlugins/App/observable/MessageOne.js
@@ -0,0 +1,3 @@
+﻿define({
+    message: "Hello, World!"
+});
--- DurandalPlugins/DurandalPlugins/App/observable/index.js
+++ DurandalPlugins/DurandalPlugins/App/observable/index.js
@@ -5,7 +5,7 @@
             moduleId: 'observable',
             fromParent: true
         }).map([
-            { route: ['', 'simple'], moduleId: 'simple', title: 'Simplest Example'},
+            { route: ['','simple'], moduleId: 'simple', title: 'Simplest Example'},
             { route: 'person', moduleId: 'person', title: 'Person'},
             { route: 'people', moduleId: 'people', title: 'People'}
         ]).buildNavigationModel();
