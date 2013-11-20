ea1468e8a48d53155a3fbb73717f5ad7d10c10b3
Michael Dudley <michael.dudley@improvingenterprises.com>
2013-11-19 23:51:56 -0600

    Replacing the observables

--- DurandalPlugins/DurandalPlugins/App/observable/person.js
+++ DurandalPlugins/DurandalPlugins/App/observable/person.js
@@ -1,17 +1,15 @@
-﻿define([], function() {
-    var first = ko.observable(''),
-                 last = ko.observable(''),
-                 fullName = ko.computed(function () {
-                     return first() + " " + last();
-                 }, this),
-                 hideWelcome = ko.computed(function () {
-                     return !first() && !last();
-                 }, this);
-
-    return {
-        first: first,
-        last: last,
-        fullName: fullName,
-        hideWelcome: hideWelcome
+﻿define(['plugins/observable'], function(observable) {
+    var vm = {
+        first: '',
+        last: ''
     };
+
+    observable.defineProperty(vm, 'fullName', function () {
+        return this.first + ' ' + this.last;
+    });
+    observable.defineProperty(vm, 'hideWelcome', function() {
+        return !this.first && !this.last;
+    });
+
+    return vm;
 });
