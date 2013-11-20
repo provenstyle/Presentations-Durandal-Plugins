696260b9cedd22c88f721544d183ffe4102dab74
Michael Dudley <michael.dudley@improvingenterprises.com>
2013-11-19 21:18:23 -0600

    Setting up the routing and observable demo

---
+++ DurandalPlugins/DurandalPlugins/App/observable/index.js
@@ -0,0 +1,16 @@
+﻿define(['plugins/router'], function(router) {
+
+    var childRouter = router.createChildRouter()
+        .makeRelative({
+            moduleId: 'observable',
+            fromParent: true
+        }).map([
+            { route: ['', 'simple'], moduleId: 'simple', title: 'Simplest Example'},
+            { route: 'person', moduleId: 'person', title: 'Person'},
+            { route: 'people', moduleId: 'people', title: 'People'}
+        ]).buildNavigationModel();
+
+    return {
+        router: childRouter
+    };
+});
---
+++ DurandalPlugins/DurandalPlugins/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DurandalPlugins.MvcApplication" Language="C#" %>
